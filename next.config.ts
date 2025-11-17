b1ef714d1066069ef1a6b743ea04fbf5b403eff2
saif <saifmb2006@gmail.com>
2025-11-17 13:50:21 +0400

    Added X-Frame-Options to config file, changed depndency list

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: 'X-Frame-Options',
-            value: 'DENY'
+            value: 'SAMEORIGIN'
           },
           {
             key: 'X-Content-Type-Options',
